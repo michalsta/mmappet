b39a7fa46f8b227fe2afcde6c0345486e1ffba44
Michał Startek <michal.startek@mimuw.edu.pl>
2025-10-31 08:57:30 +0100

    C++ reader implementation

---
+++ src/cpp/mmappet/mmappet.cpp
@@ -0,0 +1,10 @@
+#include "mmappet.h"
+
+
+int main()
+{
+auto D = OpenDataset<uint32_t,uint32_t,uint32_t, uint32_t, uint32_t>("/Users/mist/git/massimo_cpp/test1.mmappet");
+
+std::cout << D.get_column<4,uint32_t>()[6] << "\n";
+    return 0;
+}
